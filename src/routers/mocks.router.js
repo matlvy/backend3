e7ff9acccc12a29bcd667e92830b512900855342
matlvy <145528395+matlvy@users.noreply.github.com>
2024-09-19 00:07:19 -0300

    3.6.1

--- src/routers/mocks.router.js
+++ src/routers/mocks.router.js
@@ -2,7 +2,7 @@ import * as userController from "../controllers/user.controller.js";
 import { Router } from "express";
 const router = Router();
 
-router.post("/create", userController.createUser);
+router.post("/generateData", userController.createUser);
 router.get("/mockingusers", userController.getUsers);
 
 export default router;
