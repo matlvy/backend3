6f282e8c2935cd3d6dd73c24921a4c0d85d879e4
matlvy <145528395+matlvy@users.noreply.github.com>
2024-09-19 02:09:01 -0300

    3.6.2

--- src/utils/user.utils.js
+++ src/utils/user.utils.js
@@ -2,6 +2,11 @@ import bcrypt from "bcrypt";
 import { faker } from "@faker-js/faker";
 faker.locale = "es";
 
+export const createHash = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+export const isValidPassword = (user, password) =>
+  bcrypt.compareSync(password, user.password);
+
 export const generateUser = () => {
   return {
     name: faker.name.findName(),
