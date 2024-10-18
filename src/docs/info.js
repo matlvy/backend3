d8d3959140b9197f154106c3a2fd0f21277df02f
matlvy <145528395+matlvy@users.noreply.github.com>
2024-10-18 03:23:35 -0300

    3.14.9

--- src/docs/info.js
+++ src/docs/info.js
@@ -2,9 +2,9 @@ export const info = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API",
+      title: "Backend III API",
       version: "1.0.0",
-      description: "Users API",
+      description: "Platforms: Node, Express, MongoDB.",
     },
     servers: [
       {
